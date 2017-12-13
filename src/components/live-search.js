63f338d269fc57cf7e64125853cc37368a412c1c
Kayla Webb <kaylarwebb@gmail.com>
2017-12-12 20:32:32 -0500

    Basic rendering of the data.

--- src/components/live-search.js
+++ src/components/live-search.js
@@ -4,12 +4,32 @@ import SearchForm from './search-form';
 import CharacterCount from './character-count';
 import CharacterList from './character-list';
 
-export default function LiveSearch(props) {
-    return (
-        <div className="live-search">
+// export default function LiveSearch(props) {
+//     return (
+//         <div className="live-search">
+//             <SearchForm />
+//             <CharacterCount count={props.characters.length} />
+//             <CharacterList characters={props.characters} />
+//         </div>
+//     );
+// }
+// //make this stateful
+
+export default class LiveSearch extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {}
+    }
+
+    render() {
+        return (
+        <main>
             <SearchForm />
-            <CharacterCount count={props.characters.length} />
-            <CharacterList characters={props.characters} />
-        </div>
-    );
+            <CharacterCount count={this.props.characters.length} />
+            <CharacterList  characters={this.props.characters} />
+        </main>
+        )
+    }
 }
+
+//renders searchform component
